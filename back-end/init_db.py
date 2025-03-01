82d59ba1baef6f23858f6e33dda2b397876b2128
jovanoski05 <jovanoski.ivan12345@gmail.com>
2025-03-01 18:54:55 +0100

    Create users table, create db connection

---
+++ back-end/init_db.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+DATABASE = "b4s.db"
+
+def init_db():
+    with sqlite3.connect(DATABASE) as conn:
+        with open("back-end/schema.sql") as f:
+            conn.executescript(f.read())
+        print("Database initialized.")
+
+if __name__ == "__main__":
+    init_db()
---
+++ back-end/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    phoneNumber TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
