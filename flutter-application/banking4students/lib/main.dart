c4777801db353753d72e27578490f2937747d534
jovanoski05 <jovanoski.ivan12345@gmail.com>
2025-03-04 22:37:15 +0100

    Design first screen

--- flutter-application/banking4students/lib/main.dart
+++ flutter-application/banking4students/lib/main.dart
@@ -14,101 +14,70 @@ class MyApp extends StatelessWidget {
       ),
 
       debugShowCheckedModeBanner: false,
+      
+
+
       home: Scaffold(
 
-        backgroundColor: Colors.white,
-        body: Column(
-          children: [
+        backgroundColor: Color.fromARGB(255, 0, 149, 199),
 
-            Align(
-              alignment: Alignment.topLeft,
-              child: SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  height: 300,
-                  child: CustomPaint(
-                    painter: TopWave(),
-                ),
-              )
+        body: DecoratedBox(
+            decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage("assets/images/home_bg.png"), fit: BoxFit.cover),
             ),
-            
-            Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: WidgetStateProperty.resolveWith<Color?>(
-                      (Set<WidgetState> states) {
-                        if (states.contains(WidgetState.pressed)) {
-                          return Theme.of(context).colorScheme.primary;
-                        }
-                        return null; // Use the component's default.
-                      },
+            child: Center(
+              child: Column(
+                spacing: 10.5,
+                children: [
+
+                    SizedBox(height: 70,),
+                    
+                    SizedBox(
+                    width: 250,
+                    height: 250,
+                    child: DecoratedBox(decoration: BoxDecoration(
+                      image: DecorationImage(image: AssetImage('assets/images/Logo.png'), fit: BoxFit.cover)
+                    )),
                     ),
-                  ),
-                  child: const Text('Sign In', style: TextStyle(fontSize: 25.0),),
-                  onPressed: () {
-                    // ...
-                  },
-                ),
 
-                ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.resolveWith<Color?>(
-                  (Set<WidgetState> states) {
-                    if (states.contains(WidgetState.pressed)) {
-                      return Theme.of(context).colorScheme.primary;
-                    }
-                    return null; // Use the component's default.
-                  },
-                ),
+                    SizedBox(height: 250,),
+
+                     ElevatedButton(
+                        onPressed: () {},
+                        // style: ButtonStyle(elevation: MaterialStateProperty(12.0 )),
+                        style: ElevatedButton.styleFrom(
+                            elevation: 12.0,
+                            minimumSize: Size(190, 60),
+                            textStyle: TextStyle(color: Colors.black, fontSize: 35.0, fontWeight: FontWeight.bold),),
+                        child: Text('Sign In'),
+                      ),
+
+                      SizedBox(height: 25,),
+
+                       ElevatedButton(
+                        onPressed: () {},
+                        // style: ButtonStyle(elevation: MaterialStateProperty(12.0 )),
+                        style: ElevatedButton.styleFrom(
+                            elevation: 12.0,
+                            minimumSize: Size(190, 60),
+                            textStyle: const TextStyle(color: Colors.black, fontSize: 35.0, fontWeight: FontWeight.bold)),
+                        child: const Text('Register'),
+                      ),                  
+                    
+                ],
+
+
+
               ),
-              child: const Text('Register', style: TextStyle(fontSize: 25.0),),
-              onPressed: () {
-                // ...
-              },
             ),
-              ]
             ),
-          ),]
+
+
+
+        
       ),
-      ),
+      
     );
   }
 }
-
-
-//Paintings
-
-//HomePage top wave
-
-  class TopWave extends CustomPainter {
-    @override
-    void paint(Canvas canvas, Size size) {
-      Paint paint = Paint();
-      Path path = Path();
-  
-
-      // Path number 1
-  
-
-      paint.color = Color(0xff0096C7);
-      path = Path();
-      path.lineTo(size.width * 0.04, size.height * 0.03);
-      path.cubicTo(size.width * 0.04, size.height * 0.03, size.width * 0.04, size.height * 0.95, size.width * 0.04, size.height * 0.95);
-      path.cubicTo(size.width * 0.04, size.height * 0.95, size.width * 0.16, size.height * 1.1, size.width * 0.27, size.height * 0.95);
-      path.cubicTo(size.width * 0.36, size.height * 0.83, size.width * 0.45, size.height * 0.8, size.width * 0.45, size.height * 0.8);
-      path.cubicTo(size.width * 0.45, size.height * 0.8, size.width * 0.6, size.height * 0.77, size.width * 0.73, size.height);
-      path.cubicTo(size.width * 0.83, size.height * 1.14, size.width * 0.98, size.height * 0.84, size.width * 1.04, size.height * 0.69);
-      path.cubicTo(size.width * 1.04, size.height * 0.69, size.width * 1.04, size.height * 0.03, size.width * 1.04, size.height * 0.03);
-      path.cubicTo(size.width * 1.04, size.height * 0.03, size.width * 0.04, size.height * 0.03, size.width * 0.04, size.height * 0.03);
-      path.cubicTo(size.width * 0.04, size.height * 0.03, size.width * 0.04, size.height * 0.03, size.width * 0.04, size.height * 0.03);
-      canvas.drawPath(path, paint);
-    }
-    @override
-    bool shouldRepaint(CustomPainter oldDelegate) {
-      return true;
-    }
-  }
 
